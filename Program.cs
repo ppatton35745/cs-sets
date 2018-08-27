56984d2bda16a055a3eb79910c4f7c3ec60b0cfe
Philip Patton <ppatton35745@gmail.com>
2018-08-27 10:45:37 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+
+        HashSet<string> showroom = new HashSet<string>() {
+            "Viper",
+            "Corvette",
+            "Dodge",
+            "Camry"
+        };
+
+        Console.WriteLine($"there are {showroom.Count} cars in the showroom");
+
+        DisplaySet(showroom);
+
+        showroom.Add("Viper");
+
+        DisplaySet(showroom);
+
+        HashSet<string> usedLot = new HashSet<string>() {
+            "Van",
+            "Truck",
+        };
+
+        showroom.UnionWith(usedLot);
+
+        DisplaySet(showroom);
+
+        showroom.Remove("Camry");
+
+        DisplaySet(showroom);
+
+        HashSet<string> junkyard = new HashSet<string>() {
+            "Van",
+            "Truck",
+            "Tonka",
+            "BigTonka"
+        };
+
+        HashSet<string> clone = new HashSet<string>(showroom);
+
+        clone.IntersectWith(junkyard);
+
+        Console.WriteLine("Clone:");
+        DisplaySet(clone);
+
+        showroom.UnionWith(junkyard);
+
+        Console.WriteLine("My showroom with junkyard:");
+        DisplaySet(showroom);
+
+    }
+
+    private static void DisplaySet(HashSet<string> set)
+    {
+        foreach (string item in set)
+        {
+            Console.WriteLine(item);
+        }
+
+    }
+}
+
